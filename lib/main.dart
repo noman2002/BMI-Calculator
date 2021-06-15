452c68b3f915bd59695ebec7540e4408dc3b7cd9
noman2002 <nomn2002@gmail.com>
2021-06-16 00:39:09 +0530

    initial commits

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor: Colors.red,
+        primaryColor: Color(0xFF0A0E21),
         accentColor: Colors.purple,
       ),
       home: InputPage(),
