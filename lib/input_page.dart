6c207009107da714397113261da0a04d8954698b
noman2002 <nomn2002@gmail.com>
2021-06-17 00:21:32 +0530

    input page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -57,6 +57,13 @@ class _InputPageState extends State<InputPage> {
             color: Color(0xFFEB1555),
             margin: EdgeInsets.only(top: 10),
             width: double.infinity,
+            height: 80,
+            child: Text(
+              "CALCULATE YOUR BMI",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
           ),
         ],
       ),
