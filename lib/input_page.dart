317e05e020bdcea4ce86c88af191bbd52eff72c6
noman2002 <nomn2002@gmail.com>
2021-06-17 00:19:58 +0530

    input page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -53,9 +53,10 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text("Click Here"),
+          Container(
+            color: Color(0xFFEB1555),
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
           ),
         ],
       ),
