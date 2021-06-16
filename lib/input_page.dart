a0d97f025a4a8325cc0b131de6174994e6cf5bdc
noman2002 <nomn2002@gmail.com>
2021-06-16 22:02:44 +0530

    input page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class InputPage extends StatefulWidget {
@@ -13,8 +12,8 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Container(
+        color: Color(0xFF1D1E33),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
