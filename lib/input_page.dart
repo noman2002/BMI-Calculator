bec78e058fb9fe9bc1464161a038e5e5a0a57590
noman2002 <nomn2002@gmail.com>
2021-06-16 22:16:48 +0530

    input page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,12 +12,29 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Container(
-        margin: EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10),
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
