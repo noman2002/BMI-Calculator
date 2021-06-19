5fc089beaaf8940bc97cab3e13e3168fae980f4f
noman2002 <nomn2002@gmail.com>
2021-06-20 01:33:42 +0530

    input page commits

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -17,6 +17,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender? selectedGender;
+  int height = 180;
 
   @override
   Widget build(BuildContext context) {
@@ -81,13 +82,22 @@ class _InputPageState extends State<InputPage> {
                     textBaseline: TextBaseline.alphabetic,
                     children: [
                       Text(
-                        "180",
+                        height.toString(),
                         style: kNumberTextStyle,
                       ),
                       Text(
                         "cm",
                         style: kLabelTextStyle,
-                      )
+                      ),
+                      Slider(
+                          value: height.toDouble(),
+                          min: 120.0,
+                          max: 220.0,
+                          activeColor: Color(0xFFEB1555),
+                          inactiveColor: Color(0xFF8D8E98),
+                          onChanged: (double newValue) {
+                            print(newValue);
+                          }),
                     ],
                   )
                 ],
