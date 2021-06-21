3ba69ace187dc7b8537fa4318e78f0d82326c37c
noman2002 <nomn2002@gmail.com>
2021-06-21 23:49:04 +0530

    input page commits

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ void main() => runApp(BMICalculator());
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
@@ -13,7 +15,7 @@ class BMICalculator extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
 
-      
+
       home: InputPage(),
     );
   }
