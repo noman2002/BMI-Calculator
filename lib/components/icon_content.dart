2ad35fb4a36772e7e68980095480285f045c6bb5
noman2002 <nomn2002@gmail.com>
2021-06-25 19:16:08 +0530

    refractor

--- lib/components/icon_content.dart
+++ lib/components/icon_content.dart
@@ -1,6 +1,6 @@
+import 'package:bmi_calculator/constants.dart';
 import 'package:flutter/material.dart';
 
-import 'constants.dart';
 
 
 
--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -1,9 +1,10 @@
+import 'package:bmi_calculator/bottom_button.dart';
+import 'package:bmi_calculator/components/bottom_button.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
 import 'package:bmi_calculator/constants.dart';
-import 'package:bmi_calculator/input_page.dart';
 import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 
-import 'bottom_button.dart';
 
 class ResultsPage extends StatelessWidget {
   const ResultsPage({Key? key}) : super(key: key);
@@ -48,7 +49,9 @@ class ResultsPage extends StatelessWidget {
                     style: kBodyTextStyle,
                   ),
                   BottomButton(
-                    onTap: () {},
+                    onTap: () {
+                      
+                    },
                     buttonTittle: "RE-CALCULATE ",
                   )
                 ],
